1da92c594c2ab25558c830e06adabe1ff1e9333d
Vin <kevgun03@gmail.com>
2023-09-16 23:53:30 +0700

    tried out how to make a presistent notification

--- app/src/main/java/com/example/notifyyou/MainActivity.java
+++ app/src/main/java/com/example/notifyyou/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.notifyyou;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    final String channelId = "999";
+    final int notificationId = 999;
+    final String channelName = "KONTOL";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CreateNotificationChannel();
+
+        Button b = findViewById(R.id.NotificationButton);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Notification n = new NotificationCompat.Builder(MainActivity.this, channelId)
+                        .setOngoing(true)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("What the fuck")
+                        .setContentText("What the actual fuck")
+                        .build();
+
+                NotificationManagerCompat nm = NotificationManagerCompat.from(MainActivity.this);
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                nm.notify(notificationId, n);
+            }
+        });
+    }
+
+    public void CreateNotificationChannel () {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(channel);
+        }
     }
 }
