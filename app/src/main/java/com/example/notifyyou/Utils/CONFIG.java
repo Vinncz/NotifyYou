10a125469dbc09cb5456322cf720ce6aef5e61dc
Vin <kevgun03@gmail.com>
2023-09-27 23:58:34 +0700

    Added a config file: where important constant are placed

---
+++ app/src/main/java/com/example/notifyyou/Utils/CONFIG.java
@@ -0,0 +1,9 @@
+package com.example.notifyyou.Utils;
+
+public class CONFIG {
+    public static final String channelId = "NotifyYouNotificationChannelId";
+    public static final String channelName = "NotifyYouNotificationChannelName";
+    public static final String sharedPreferencePinnedTileItemsChannel = "NotifyYouPinnedTileItemsChannel";
+    public static final String sharedPreferenceUnpinnedTileItemsChannel = "NotifyYouUnpinnedTileItemsChannel";
+
+}
