100b91813251ff3ab83da69724bfb825d841cfe7
Vin <kevgun03@gmail.com>
2023-09-29 22:36:10 +0700

    Found out why the Retrieve method must use FromJson() or else it may explode, and wrote an explanation for it.

--- app/src/main/java/com/example/notifyyou/Repositories/TileItemRepository.java
+++ app/src/main/java/com/example/notifyyou/Repositories/TileItemRepository.java
@@ -34,22 +34,19 @@ public class TileItemRepository {
     }
 
     public ArrayList<TileItem> GetAllPinned () {
-        // masih gapaham kenapa perlu di retrieve sebagai string dulu
-        String tis = Retrieve(CONFIG.sharedPreferencePinnedTileItemsChannel, "", String.class);
-        Log("YANG DIDAPET DARI GETALLPINNED:\n" + tis);
-
-        return FromJson(tis, ArrayListWithTileItemType, new ArrayList<TileItem>());
+        return FromJson(
+                Retrieve(CONFIG.sharedPreferencePinnedTileItemsChannel, ""),
+                ArrayListWithTileItemType,
+                new ArrayList<TileItem>()
+        );
     }
 
     public ArrayList<TileItem> GetNonPinned () {
-        // masih gapaham kenapa perlu di retrieve sebagai string dulu
-        String tis = Retrieve(CONFIG.sharedPreferenceUnpinnedTileItemsChannel, "", String.class);
-        Log("YANG DIDAPET DARI SHAREDPREFERENCES:\n" + tis);
-
-        ArrayList items = FromJson(tis, ArrayListWithTileItemType, new ArrayList<TileItem>());
-        Log("KALO [YANG DIDAPET TADI] DIUBAH DARI [JSON] KE [OBJECTNYA]:\n" + ToJson(items));
-
-        return items;
+        return FromJson(
+                Retrieve(CONFIG.sharedPreferenceUnpinnedTileItemsChannel, ""),
+                ArrayListWithTileItemType,
+                new ArrayList<TileItem>()
+        );
     }
 
     public TileItem Get (int _tileId) {
@@ -57,9 +54,17 @@ public class TileItemRepository {
         return null;
     }
 
+    /*
+     * (!) WARNING
+     *
+     * YANG BERTANGGUNG JAWAB UNTUK MEMASTIKAN BAHWA
+     * VALUE BARU TIDAK MENG-OVERWRITE YANG LAMA
+     * ADALAH MEREKA YANG MEMANGGIL METHOD PUT.
+     */
     public Boolean Post (TileItem ti) {
         String json = ToJson(ti);
-        Log("YANG BARU DISUBMIT:\n" + json);
+
+        Log("NEWLY SUBMITTED DATA:\n" + json);
 
         ArrayList<TileItem> existingItems = GetNonPinned();
 
@@ -67,7 +72,9 @@ public class TileItemRepository {
 
         json = ToJson(existingItems);
 
-        return Put(CONFIG.sharedPreferenceUnpinnedTileItemsChannel, json);
+        Write(CONFIG.sharedPreferenceUnpinnedTileItemsChannel, json);
+
+        return CommitChanges();
     }
 
     private void Log (String _s) {
@@ -82,28 +89,53 @@ public class TileItemRepository {
         return g.toJson(_value);
     }
 
-    private <T> T FromJson (String _value, Type _class) {
-        return g.fromJson(_value, _class);
-    }
-
     private <T> T FromJson (String _value, Type _class, Object _fallbackValue) {
         Object o = g.fromJson(_value, _class);
         return (T) (o != null ? o : _fallbackValue);
     }
 
-    private <T> T Retrieve (String _resourcePath, Object _fallbackValue, Type _resourceClass) {
-        String retrievedData = db.getString(_resourcePath, ToJson(_fallbackValue));
-        return FromJson(retrievedData, _resourceClass);
+    private String Retrieve (String _resourcePath, Object _fallbackValue) {
+        /*
+        *
+        * RETRIEVAL SEQUENCE FOR DATA THAT ARE INSIDE SHARED PREFERENCES:
+        *
+        * 1. Look for data stored in the specified path, and retrieve it.
+        *      (Here, the specified path is represented by the `_resourcePath` param)
+        *
+        * 2. Due to the saved data being encoded in JSON, it needs to be unwrapped first into a string.
+        *      (Here, you call the FromJson() method to convert the retrieved JSON into a String object)
+        *
+        * 3. You return the converted value.
+        */
+        return FromJson (
+                    db.getString(_resourcePath, ToJson(_fallbackValue)),
+                    String.class,
+                    _fallbackValue
+               );
     }
 
-    private <T> Boolean Put (String _placementPath, String _value) {
+    private <T> void Write (String _placementPath, String _value) {
+
+        /* TODO: HERE SHOULD NOT BE ANYMORE VALIDATION BEYOND THIS POINT */
+
         /* Sanitize the data */
         String preparedData;
-        try { preparedData = ToJson(_value); } catch (Error e) { System.out.println(e.toString()); return false;}
+        try {
+            /* If the provided data is correct, then the assignment should proceed normally */
+            preparedData = ToJson(_value);
+        } catch (Error e) {
+            /* If its throwing an Error, then you know that the param you receive is invalid, and you must terminate the SP writing */
+            Log("CAUGHT ERROR WHEN WRITING INTO SHARED PREFERENCE" + e.toString());
+//            throw e;
+
+            return;
+        }
 
         /* Put the data into SharedPreference */
         editor.putString(_placementPath, preparedData);
+    }
 
+    public Boolean CommitChanges () {
         return editor.commit();
     }
 
